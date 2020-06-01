fc7448b456ee6b21c0ce0a639f5727e6ec5bff5c
ocampoaleja <omaira.527@hotmail.com>
2020-06-01 14:56:34 -0500

    feature. connecting react-redux

---
+++ src/components/CantidadPokemon.jsx
@@ -0,0 +1,14 @@
+import React, { Component} from 'react'
+
+class CantidadPokemon extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                Unidades:30
+            </React.Fragment>
+        )
+    }
+}
+
+
+export default CantidadPokemon;
